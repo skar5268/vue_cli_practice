68a0cd37b59dd70cbb2f76500149852144c4e485
skar5268 <skar5268@gmail.com>
2021-05-23 18:22:14 +0800

    changes

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+  {{ name }}
+  <button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap";
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <card></card>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
